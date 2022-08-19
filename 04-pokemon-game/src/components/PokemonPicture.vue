adb841814bd8d9a36e40917e8a6e22456899cfee
INGENIERO3 <INGENIERO3@pyt.local>
2022-08-19 16:42:10 -0500

    pokemon game

---
+++ 04-pokemon-game/src/components/PokemonPicture.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="pokemon-container">
+        <img class="hidden-pokemon" :src="imgSrc" alt="Pokemon">
+        <img v-if="showPokemon" class="fade-in" :src="imgSrc" alt="Pokemon">
+    </div>
+</template>
+
+<script>
+
+// Number. required
+
+export default {
+    name: 'PokemonPicture',
+    computed: {
+        imgSrc(){
+            return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${ this.pokemonId }.svg`
+        }
+    },
+    props:{
+        pokemonId: {
+            type: Number,
+            required: true
+        },
+        showPokemon: {
+            type: Boolean,
+            required: true,
+            default: false
+        }
+    }
+
+}
+</script>
+
+<style scoped>
+
+.pokemon-container {
+    height: 200px;
+}
+img {
+    height: 200px;
+    position: absolute;
+    right: 32%;
+    user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    -webkit-user-drag: none;
+    -webkit-user-select: none;
+}
+.hidden-pokemon {
+    filter: brightness(0);
+}
+
+</style>
---
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>¿Quién es este pokémon?</h1>
+    <!-- TODO: img -->
+    <PokemonPicture :pokemonId='4' :showPokemon="true" />
+    <!-- TODO: Opciones -->
+    <PokemonOptions />
+</template>
+
+<script>
+import PokemonOptions from '@/components/PokemonOptions.vue';
+import PokemonPicture from '@/components/PokemonPicture.vue'
+
+export default {
+    name: "PokemonPage",
+    components: { 
+        PokemonPicture, 
+        PokemonOptions    
+    }
+}
+</script>
